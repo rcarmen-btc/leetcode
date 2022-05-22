e520e008607d094cb9a5578f1584a3e13bff797c
riserlarenss <73555480+rcarmen-btc@users.noreply.github.com>
2022-05-23 02:00:27 +0300

    Time: 513 ms (5.00%), Space: 15.4 MB (97.81%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,15 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        count = len(nums)
-        mid = count // 2
-        start = 0
-        end = count
-        if target not in nums:
+        try:
+            res = nums.index(target)
+        except:
             return -1
-        while target != nums[mid]:
-            if target >= nums[mid]:
-                start = mid
-            else:
-                end = mid
-            mid = start + (end - start) // 2
-        return mid 
+        return res
