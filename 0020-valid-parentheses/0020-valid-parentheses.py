76031d71548ee1d3c17c804f5b94924431357c81
riserlarenss <73555480+rcarmen-btc@users.noreply.github.com>
2022-11-01 16:25:32 +0300

    Time: 41 ms (24.44%), Space: 14 MB (11.88%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,12 +8,15 @@ class Solution(object):
         stack = []
         
         stack.append(s[0])
+        ppc = 1
         for i in range(1, len(s)):
             if len(stack) > 0 and s[i] == br.get(stack[-1], ''):
                 stack.pop()
+                ppc -= 1
             else:
                 stack.append(s[i])
-        if len(stack) == 0:
+                ppc += 1
+        if ppc == 0:
             return True
         return False
              
